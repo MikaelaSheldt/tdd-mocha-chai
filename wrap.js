6cabb897947150568959181410fc107d5a58c9a3
Mikaela Sheldt <msheldt@gmail.com>
2019-06-25 15:48:15 -0400

    ADD(files) also added basic code for wrap and wrap.spec

---
+++ wrap.js
@@ -0,0 +1,6 @@
+/*
+takes two arguments: a string, and a column number. The function returns the string, but with line breaks inserted at just the right places to make sure that no line is longer than the column number.
+*/
+
+const wrap = (line, maxLength) => "";
+module.exports = wrap;
