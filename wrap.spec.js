e78225861e047c47912e9281d8bc0affa845affa
Mikaela Sheldt <msheldt@gmail.com>
2019-06-25 16:10:05 -0400

    ADD(wrap.spec.js) add more tests before writing solution

--- wrap.spec.js
+++ wrap.spec.js
@@ -5,4 +5,14 @@ describe('wrap', () => {
   it('Returns empty string if empty string was provided', () => {
     expect(wrap("", 10)).to.equal("");
   });
+  it('Returns the text if the text is shorter than the maxLength', () => {
+    expect(wrap('hello world', 20)).to.equal('hello world')
+  });
+  it('Returns the text if the text length is the same as the maxLength', () => {
+    expect(wrap('hello world', 11)).to.equal('hello world')
+  });
+
+  it('splits a continuous string on the maxLength', () => {
+    expect(wrap('aaaaaaaaaa', 2)).to.equal('aa\naa\naa\naa\naa')
+  });
 });
